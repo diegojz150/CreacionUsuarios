fcf5e729283306de128e7c46a55ee76d011ec5d0
Diego Jose Zuniga Garcia <diegojz150@gmail.com>
2015-05-28 18:45:45 -0500

    Mayo 28

--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'application#index'
+   #get 'users#index'
+   resources :users #Por defecto lista de crut
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
