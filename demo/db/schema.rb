97273b9f0ffd0a52660ed87d9aa91b65bb737126
Felipe <felipe@MacBook-de-Felipe.local>
2015-05-29 02:50:53 -0500

    Cambio Calidoso

--- demo/db/schema.rb
+++ demo/db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(version: 20150527023426) do
     t.string   "name"
     t.string   "email"
     t.string   "password"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
